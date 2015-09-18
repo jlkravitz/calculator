23f8bbc2ffa5e27674c87e4a6d2d90b1dc069e70
Joshua Kravitz <jlkravitz@gmail.com>
2015-09-18 13:27:51 -0700

    Fix warnings

--- ExprParser.hs
+++ ExprParser.hs
@@ -41,25 +41,25 @@ op = chToOp <$> (char '+' <|> char '-' <|> char '*' <|> char '/')
 
 buildExpr :: [Expr] -> [Operator] -> Expr
 buildExpr (first:operands) operators = evalExpr' operands operators [first] []
-  where evalExpr' (x:xs) (op:ops)  litStack opStack = evalExpr' xs ops (x:litStack') (op:opStack')
-          where (litStack', opStack') = combineHigherPrecedence litStack opStack op
+  where evalExpr' (x:xs) (y:ys)  litStack opStack = evalExpr' xs ys (x:litStack') (y:opStack')
+          where (litStack', opStack') = combineHigherPrecedence litStack opStack y
         evalExpr' [] [] litStack opStack = combineExpressions litStack opStack
         evalExpr' _ _ _ _ = error "wtf"
 buildExpr _ _ = error "wtf"
 
 combineHigherPrecedence :: [Expr] -> [Operator] -> Operator -> ([Expr], [Operator])
-combineHigherPrecedence (x:y:xs) (op:ops) curOp
-  | precedence op >= precedence curOp = combineHigherPrecedence (Op op x y:xs) ops curOp
-combineHigherPrecedence xs ops _ = (xs, ops)
+combineHigherPrecedence (a:b:xs) (y:ys) curOp
+  | precedence y >= precedence curOp = combineHigherPrecedence (Op y a b:xs) ys curOp
+combineHigherPrecedence xs ys _ = (xs, ys)
 
 combineExpressions :: [Expr] -> [Operator] -> Expr
-combineExpressions (x:y:exprs) (op:ops) = combineExpressions ((Op op x y):exprs) ops
+combineExpressions (a:b:exprs) (y:ys) = combineExpressions ((Op y a b):exprs) ys
 combineExpressions (x:[]) [] = x
 combineExpressions _ _ = error "wtf"
 
 evalExpr :: Expr -> Integer
 evalExpr (Const x) = x
-evalExpr (Op op a b) = performOperation op (evalExpr a) (evalExpr b)
+evalExpr (Op y a b) = performOperation y (evalExpr a) (evalExpr b)
 
 precedence :: Operator -> Integer
 precedence Add = 0
