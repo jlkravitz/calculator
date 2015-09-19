75a091daa84d6bffc44e432866fc10224864a0ea
Joshua Kravitz <jlkravitz@gmail.com>
2015-09-19 10:18:50 -0700

    Make it easier to add operators

--- Expr.hs
+++ Expr.hs
@@ -1,19 +1,32 @@
 {-# OPTIONS_GHC -Wall #-}
 module Expr (parseExpr, evalExpr) where
 
-import Data.List
 import Control.Applicative ((<$>), (*>), (<*))
-import Text.ParserCombinators.Parsec
-import Data.Maybe
+import Text.ParserCombinators.Parsec hiding (token)
+import qualified Data.Map as M
 
 
 ---------- Data Types ----------
 
-data Operator = Add | Sub | Mul | Div deriving Show
+data Operator = Operator {
+  precedence :: Int,
+  token :: Char,
+  binaryFn :: (Integer -> Integer -> Integer)
+}
 data Expr = Literal Integer
           | Op Operator Expr Expr
-          deriving Show
 
+allOperators :: [Operator]
+allOperators = [
+  Operator 0 '+' (+),
+  Operator 0 '-' (-),
+  Operator 1 '*' (*),
+  Operator 1 '/' quot,
+  Operator 2 '^' (^)]
+
+-- Return map from key of operator to the operators themselves
+mapOn :: Ord a => (Operator -> a) -> M.Map a Operator
+mapOn f = M.fromList $ zip (map f allOperators) allOperators
 
 ---------- Parsing Expressions with Parsec ----------
 
@@ -56,10 +69,9 @@ pair = do
   return (o, e)
 
 op :: Parser Operator
-op = chToOp <$> (char '+' <|> char '-' <|> char '*' <|> char '/')
+op = chToOp <$> (oneOf $ map token allOperators)
   where chToOp :: Char -> Operator
-        chToOp c = snd $ fromJust $ find ((== c) . fst) table
-        table = [('+', Add), ('-', Sub), ('*', Mul), ('/', Div)]
+        chToOp c = mapOn token M.! c
 
 
 ----------  Building Expressions with the Shunting-Yard algorithm ----------
@@ -77,12 +89,6 @@ combineHigherPrecedence (a:b:xs) (y:ys) curOp
   | precedence y >= precedence curOp = combineHigherPrecedence (Op y a b:xs) ys curOp
 combineHigherPrecedence xs ys _ = (xs, ys)
 
-precedence :: Operator -> Integer
-precedence Add = 0
-precedence Sub = 0
-precedence Mul = 1
-precedence Div = 1
-
 combineExpressions :: [Expr] -> [Operator] -> Expr
 combineExpressions (a:b:exprs) (y:ys) = combineExpressions ((Op y a b):exprs) ys
 combineExpressions (x:[]) [] = x
@@ -93,11 +99,5 @@ combineExpressions _ _ = error "Something went horribly wrong!"
 
 evalExpr :: Expr -> Integer
 evalExpr (Literal x) = x
-evalExpr (Op y a b) = performOperation y (evalExpr a) (evalExpr b)
-
-performOperation :: Operator -> Integer -> Integer -> Integer
-performOperation Add x y = y + x
-performOperation Sub x y = y - x
-performOperation Mul x y = y * x
-performOperation Div x y = y `quot` x
+evalExpr (Op y a b) = (binaryFn y) (evalExpr b) (evalExpr a)
 
